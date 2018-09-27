6017d5c9170bbd6cc63dc06914c0316b920c33ce
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-09-27 21:56:45 +0900

    Add Post model

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,25 @@ import 'package:http/http.dart' as http;
 
 void main() => runApp(new MyApp());
 
+// Model: Post
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId, this.id, this.title, this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
